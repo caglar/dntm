8252e750058bb634b9aa389637c568734fcc30d5
caglar <ca9lar@gmail.com>
2017-05-09 16:00:08 -0400

    Added the final scripts of the model.

--- scripts/create_commands_babi_ffntm.py
+++ scripts/create_commands_babi_ffntm.py
@@ -1,12 +1,20 @@
 import numpy
+import argparse
+
+parser = argparse.ArgumentParser("Parameters for the single soft model.")
+parser.add_argument("--task_id", default=1, type=int)
+
+args = parser.parse_args()
 
 numpy.random.seed(1)
+
 ppath="export PYTHONPATH=/home/gulcehre/code/python/dntm/codes/:$PYTHONPATH; "
+
 save_path = "/rap/jvb-000-aa/gulcehre/dntm/ff_exps/"
-save_file = "commands_babi_ff_ntm.txt"
+save_file = "commands_babi_ff_ntm_taskid_{0}.txt".format(args.task_id)
 
 command_template = \
-"""THEANO_FLAGS='floatX=float32,device=cuda,force_device=True,lib.cnmem=0.92,mode=FAST_RUN' python -u submit_ff_soft_nodb.py --seed {0} --save_path {1} --seed {2}"""
+"""THEANO_FLAGS='floatX=float32,device=cuda,force_device=True,lib.cnmem=0.92,mode=FAST_RUN' python -u submit_ff_soft_nodb.py --seed {0} --save_path {1} --task_id {2}"""
 
 command_template = ppath + command_template
 cmds = []
@@ -14,12 +22,10 @@ cmds = []
 njobs = 24
 rnd_int = numpy.random.randint
 
-task_ids = numpy.arange(20)
 seeds = numpy.arange(5, 15)
 
-for task_id in task_ids:
-    for seed in seeds:
-        cmds.append(command_template.format(seed, save_path, task_id))
+for seed in seeds:
+    cmds.append(command_template.format(seed, save_path, args.task_id))
 
 try:
     with open(save_file, "w") as file:
--- scripts/launch_jobs.sh
+++ scripts/launch_jobs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -x
-
-smart-dispatch -t 6:00:00 -q gpu_1 -r --pbsFlags="-lfeature=k80 -Ajvb-000-ag" -g 1 -f commands_babi_ff_ntm.txt launch
+CMDFILE=$1
+smart-dispatch -t 6:00:00 -q gpu_1 -r --pbsFlags="-lfeature=k80 -Ajvb-000-ag" -g 1 -f $CMDFILE launch
